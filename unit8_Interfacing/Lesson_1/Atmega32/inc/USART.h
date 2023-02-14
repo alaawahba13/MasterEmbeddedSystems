e03fd82e372c4ca4ddf8103cf5a5875750554e4d
Alaa Wahba <alaawahbaa13@gmail.com>
2023-02-14 22:19:14 +0200

    MCAL

---
+++ unit8_Interfacing/Lesson_1/Atmega32/inc/USART.h
@@ -0,0 +1,38 @@
+/*
+ * USART.h
+ *
+ *  Created on: 13 Feb 2023
+ *      Author: Alaa Wahba
+ */
+
+#ifndef ATMEGA32_INC_USART_H_
+#define ATMEGA32_INC_USART_H_
+//-----------------------------
+//Includes
+//-----------------------------
+#include "Atmega32.h"
+#include "BIT_MATH.h"
+
+
+/*
+* ===============================================
+* APIs Supported by "MCAL DIO DRIVER"
+* ===============================================
+*/
+
+void USART_init(void);
+void USART_send(uint8 data);
+uint8 USART_recieve();
+
+void USART_sendString(char *str);
+void USART_sendNumber(uint32 data);
+
+char* USART_recieveString();
+uint32 USART_recieveNumber();
+
+
+
+
+
+
+#endif /* ATMEGA32_INC_USART_H_ */
